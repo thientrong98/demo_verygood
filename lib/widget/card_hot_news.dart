15f3771eb83805276f5731fadb941a0a1e1b7469
Tran Thien Trong <trongtt7@fpt.com.vn>
2021-05-07 16:57:14 +0700

    add some line code

--- lib/widget/card_hot_news.dart
+++ lib/widget/card_hot_news.dart
@@ -15,19 +15,21 @@ class CardHotNew extends StatefulWidget {
 class _CardHotNewState extends State<CardHotNew> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Text(
-          widget.title != null ? widget.title : "",
-          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-        ),
-        SizedBox(height: 5),
-        Text(
-          widget.subtitle != null ? widget.subtitle : "",
-          style: TextStyle(color: Colors.grey),
-        ),
-      ],
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(
+            widget.title != null ? widget.title : "",
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 5),
+          Text(
+            widget.subtitle != null ? widget.subtitle : "",
+            style: TextStyle(color: Colors.grey),
+          ),
+        ],
+      ),
     );
   }
 }
